53f40244888056b920a63d8fb94cb6fe26c9c163
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-03-07 07:56:50 -0500

    Add storedOutput response

--- minimaxsumgame/models.py
+++ minimaxsumgame/models.py
@@ -2,6 +2,6 @@ from django.db import models
 # Create your models here.
 class MinimaxPack(models.Model):
     inputs = models.CharField
-
+    storedOutputs = models.CharField
     def __repr__(self):
-        return '<MinimaxPack object (For inputs [{}])>'.format(self.inputs)
+        return '<MinimaxPack object (For inputs [{}], Output is: {})>'.format(self.inputs, self.storedOutputs)
